583e80c9c3a58785b609c1f21a7aabe82ce8f28c
no_author <contact@osivia.com>
2015-09-17 08:30:40 +0000

    Prepare for CNS release (dependency on opentoutatice 3.1.2) NOTE: classes RoutingNotificationsSwitch and RoutingNotificationsVeto must be simplified with utlity method of core when dependency on opentoutatice will be changed (version > 3.1.2)

--- opentoutatice-addon-portal-notifications-ecm/src/main/java/org/osivia/platform/portal/notifications/veto/RoutingNotificationsVeto.java
+++ opentoutatice-addon-portal-notifications-ecm/src/main/java/org/osivia/platform/portal/notifications/veto/RoutingNotificationsVeto.java
@@ -18,17 +18,22 @@
  */
 package org.osivia.platform.portal.notifications.veto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
+import org.nuxeo.ecm.core.api.UnrestrictedSessionRunner;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.platform.ec.notification.NotificationListenerVeto;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoute;
 
-import fr.toutatice.ecm.platform.core.helper.ToutaticeWorkflowHelper;
+import fr.toutatice.ecm.platform.core.helper.ToutaticeQueryHelper;
 
 
 /**
@@ -36,7 +41,12 @@ import fr.toutatice.ecm.platform.core.helper.ToutaticeWorkflowHelper;
  *
  */
 public class RoutingNotificationsVeto implements NotificationListenerVeto {
-
+    
+    public static final String GET_WF_ON_DOCUMENT_QUERY = "select * from DocumentRoute where docri:participatingDocuments = '%s' "
+            + "and ecm:currentLifeCycleState IN ('ready','running') order by dc:created";
+    
+    // FIXME: restore with just ToutaticeWorkflowHelper.getWorkflowsOnDocument 
+    // when dependence on opentoutatice changed (version > 3.1.2)
     @Override
     public boolean accept(Event event) throws Exception {
         boolean accept = true;
@@ -46,12 +56,66 @@ public class RoutingNotificationsVeto implements NotificationListenerVeto {
             DocumentEventContext docCtx = (DocumentEventContext) event.getContext();
             DocumentModel sourceDocument = docCtx.getSourceDocument();
 
-            List<DocumentRoute> workflowsOnDocument = ToutaticeWorkflowHelper.getWorkflowsOnDocument(sourceDocument);
+            List<DocumentRoute> workflowsOnDocument = getWorkflowsOnDocument(sourceDocument);
             accept = CollectionUtils.isEmpty(workflowsOnDocument);
 
         }
 
         return accept;
     }
+    
+    /**
+     * @param currentDoc
+     * @return workflows on document.
+     */
+    public static List<DocumentRoute> getWorkflowsOnDocument(DocumentModel currentDoc){
+        List<DocumentRoute> routes = new ArrayList<DocumentRoute>(1);
+        
+        CoreSession session = currentDoc.getCoreSession();
+        String query = String.format(GET_WF_ON_DOCUMENT_QUERY, currentDoc.getId());
+        
+        UnrestrictedQueryRunner queryRunner = new UnrestrictedQueryRunner(session, query);
+        DocumentModelList wfs = queryRunner.runQuery();
+        
+        if(CollectionUtils.isNotEmpty(wfs)){
+            for(DocumentModel wf : wfs){
+              routes.add(wf.getAdapter(DocumentRoute.class));
+            }
+        }
+
+        return routes;
+    }
+    
+    /**
+     * To query in unrestricted mode.
+     * 
+     * @author David Chevrier.
+     *
+     */
+    public static class UnrestrictedQueryRunner extends UnrestrictedSessionRunner {
+
+        String query;
+
+        DocumentModelList docs;
+
+        protected UnrestrictedQueryRunner(CoreSession session, String query) {
+            super(session);
+            this.query = query;
+        }
+
+        @Override
+        public void run() throws ClientException {
+            docs = session.query(query);
+            for (DocumentModel documentModel : docs) {
+                documentModel.detach(true);
+            }
+        }
+
+        public DocumentModelList runQuery() throws ClientException {
+            runUnrestricted();
+            return docs;
+        }
+    }
+
 
 }
