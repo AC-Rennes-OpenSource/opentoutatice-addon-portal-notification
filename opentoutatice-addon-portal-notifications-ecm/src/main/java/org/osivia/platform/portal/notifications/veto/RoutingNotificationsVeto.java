887851e849e6d56cadad11255d6571df29ba2264
no_author <contact@osivia.com>
2015-09-21 07:54:59 +0000

    use of getWorkflowsOnDocument utility method added

--- opentoutatice-addon-portal-notifications-ecm/src/main/java/org/osivia/platform/portal/notifications/veto/RoutingNotificationsVeto.java
+++ opentoutatice-addon-portal-notifications-ecm/src/main/java/org/osivia/platform/portal/notifications/veto/RoutingNotificationsVeto.java
@@ -34,6 +34,7 @@ import org.nuxeo.ecm.platform.ec.notification.NotificationListenerVeto;
 import org.nuxeo.ecm.platform.routing.api.DocumentRoute;
 
 import fr.toutatice.ecm.platform.core.helper.ToutaticeQueryHelper;
+import fr.toutatice.ecm.platform.core.helper.ToutaticeWorkflowHelper;
 
 
 /**
@@ -45,8 +46,6 @@ public class RoutingNotificationsVeto implements NotificationListenerVeto {
     public static final String GET_WF_ON_DOCUMENT_QUERY = "select * from DocumentRoute where docri:participatingDocuments = '%s' "
             + "and ecm:currentLifeCycleState IN ('ready','running') order by dc:created";
     
-    // FIXME: restore with just ToutaticeWorkflowHelper.getWorkflowsOnDocument 
-    // when dependence on opentoutatice changed (version > 3.1.2)
     @Override
     public boolean accept(Event event) throws Exception {
         boolean accept = true;
@@ -56,7 +55,7 @@ public class RoutingNotificationsVeto implements NotificationListenerVeto {
             DocumentEventContext docCtx = (DocumentEventContext) event.getContext();
             DocumentModel sourceDocument = docCtx.getSourceDocument();
 
-            List<DocumentRoute> workflowsOnDocument = getWorkflowsOnDocument(sourceDocument);
+            List<DocumentRoute> workflowsOnDocument = ToutaticeWorkflowHelper.getWorkflowsOnDocument(sourceDocument);
             accept = CollectionUtils.isEmpty(workflowsOnDocument);
 
         }
@@ -64,58 +63,5 @@ public class RoutingNotificationsVeto implements NotificationListenerVeto {
         return accept;
     }
     
-    /**
-     * @param currentDoc
-     * @return workflows on document.
-     */
-    public static List<DocumentRoute> getWorkflowsOnDocument(DocumentModel currentDoc){
-        List<DocumentRoute> routes = new ArrayList<DocumentRoute>(1);
-        
-        CoreSession session = currentDoc.getCoreSession();
-        String query = String.format(GET_WF_ON_DOCUMENT_QUERY, currentDoc.getId());
-        
-        UnrestrictedQueryRunner queryRunner = new UnrestrictedQueryRunner(session, query);
-        DocumentModelList wfs = queryRunner.runQuery();
-        
-        if(CollectionUtils.isNotEmpty(wfs)){
-            for(DocumentModel wf : wfs){
-              routes.add(wf.getAdapter(DocumentRoute.class));
-            }
-        }
-
-        return routes;
-    }
-    
-    /**
-     * To query in unrestricted mode.
-     * 
-     * @author David Chevrier.
-     *
-     */
-    public static class UnrestrictedQueryRunner extends UnrestrictedSessionRunner {
-
-        String query;
-
-        DocumentModelList docs;
-
-        protected UnrestrictedQueryRunner(CoreSession session, String query) {
-            super(session);
-            this.query = query;
-        }
-
-        @Override
-        public void run() throws ClientException {
-            docs = session.query(query);
-            for (DocumentModel documentModel : docs) {
-                documentModel.detach(true);
-            }
-        }
-
-        public DocumentModelList runQuery() throws ClientException {
-            runUnrestricted();
-            return docs;
-        }
-    }
-
 
 }
